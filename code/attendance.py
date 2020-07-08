39c7c16c69086c429d1d2499252d48dd9ceac736
Ariana <ariana.lguimaraes@ufrpe.br>
2020-07-08 00:34:08 -0300

    starting webpage

--- code/attendance.py
+++ code/attendance.py
@@ -1,18 +1,20 @@
-f = open("Meet Attendance - Attendance.csv", "r")
-f.readline()
-f.readline()
 
-file = f.read()
-f.close()
+def attendance(file_name):
+    f = open("..\\data\\%s" % file_name, "r")
+    f.readline()
+    f.readline()
 
-file = file.replace("\n", ",").split(",")
-students = {}
-for person in file:
-    if len(person) > 0:
-        if person not in students.keys():
-            students[person] = 1
-        else:
-            students[person] += 1
-for person in students:
-    if students[person] >= 3:
-        print(person)
+    file = f.read()
+    f.close()
+
+    file = file.replace("\n", ",").split(",")
+    students = {}
+    for person in file:
+        if len(person) > 0:
+            if person not in students.keys():
+                students[person] = 1
+            else:
+                students[person] += 1
+    for person in students:
+        if students[person] >= 3:
+            print(person)
